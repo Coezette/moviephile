2c4cc7dca02c42e7d1a970c7a93ddea77338fa50
Coezette Sibanda <cs3353@nyu.edu>
2020-11-10 10:41:26 +0400

    minor commit

---
+++ test/movies_repository_test.dart
@@ -0,0 +1,21 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:moviephile/blocs/popular_movies_bloc.dart';
+import 'package:moviephile/data_providers/movies_provider.dart';
+import 'package:moviephile/events/movies_events.dart';
+import 'package:moviephile/models/genre.dart';
+import 'package:moviephile/models/person_response.dart';
+import 'package:moviephile/models/popular_movies_rs.dart';
+import 'package:moviephile/repository/movies_repository.dart';
+import 'package:moviephile/states/popular_movies_state.dart';
+
+class MockPopularMoviesRS extends Mock implements PopularMoviesRS {}
+
+class MockMoviesProvider extends Mock implements MoviesProvider {}
+
+void main() {
+//  test("repo..gets popular movies", () async {
+//    MoviesProvider _provider = MoviesProvider();
+//  });
+}
